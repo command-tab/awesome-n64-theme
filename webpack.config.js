51e652425bed38b7c68f61c9ab4d47ca3ccc090c
Collin Allen <collin@command-tab.com>
2020-05-04 10:44:53 -0700

    Set webpack dev server listen host

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, outputDir),
     compress: true,
-    port: 9000
+    port: 9000,
+    host: '0.0.0.0'
   },
   plugins: [
     new CopyPlugin([
